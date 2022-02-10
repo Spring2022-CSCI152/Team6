8b840111aa821187675ba08c7ff2e01bc9986692
Gurleen kaur <gurleenkaur@Gurleens-iMac.local>
2022-02-10 11:17:33 -0800

    changing the format for app.js and pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import spinner from './assets/spinner.svg';
-import logo from './assets/roadmap.jpg';
+import React from 'react'; 
 import './App.css';
-import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import Login from './pages/login';
+
+
+
+export const AuthContext = React.createContext()
 
 function App() {
-  window.onload = function(){
-    setInterval(function(){
-        // alert("Hello");
-    }, 5000);
- };
+  //const [authState, setAuthState] = useState(localStorage.getItem("user") ? true : false)
+
   return (
-    <div className="App">
-      <style>
-      @import url('https://fonts.googleapis.com/css2?family=Quintessential&display=swap');
-      </style>
-      <Header/>
-      <header className="App-header">
-        <p>Welcome to Roadmap</p>
-        <img src={spinner} width="15%" className="spinner" alt="logo" />
-        <img src={logo} height="auto"/>
-        <p1>Set your education path ...</p1>
-      </header>
-    </div>
+    <Router>
+          
+          {/* <AuthContext.Provider value={[authState, setAuthState]}> */}
+          <Routes>
+              <Route path="/" exact component={Home} />
+              <Route path="/login" component={Login} />
+           </Routes>
+           {/* </AuthContext.Provider> */}
+    </Router>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import spinner from '/Users/gurleenkaur/Desktop/Team6/src/assets/spinner.svg';
+import logo from '/Users/gurleenkaur/Desktop/Team6/src/assets/roadmap.jpg';
+import '/Users/gurleenkaur/Desktop/Team6/src/App.css';
+import Header from '/Users/gurleenkaur/Desktop/Team6/src/components/Header.js';
+
+function Home() {
+  window.onload = function(){
+    setInterval(function(){
+        // alert("Hello");
+    }, 5000);
+ };
+  return (
+    <div className="App">
+      <style>
+      @import url('https://fonts.googleapis.com/css2?family=Quintessential&display=swap');
+      </style>
+      <Header/>
+      <header className="App-header">
+        <p>Welcome to Roadmap</p>
+        <img src={spinner} width="15%" className="spinner" alt="logo" />
+        <img src={logo} height="auto"/>
+        <p1>Set your education path ...</p1>ß
+      </header>
+    </div>
+  );
+}
+
+export default Home;
