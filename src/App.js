fe9a66204516e4d910e7a270a17d3e07a9c156e3
gurleen227 <gurleenkaurvaid@gmail.com>
2022-02-10 17:32:57 -0800

    routes are partially working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react'; 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages';
 import Login from './pages/login';
 import SignUp from './pages/signup';
+import {useState} from 'react';
 
 
 
-// export const AuthContext = React.createContext()
+//  export const AuthContext = React.createContext()
 
 function App() {
-  //const [authState, setAuthState] = useState(localStorage.getItem("user") ? true : false)
+  // const [authState, setAuthState] = useState(localStorage.getItem("user") ? true : false)
 
   return (
     <Router>
           
           {/* <AuthContext.Provider value={[authState, setAuthState]}> */}
-          <Routes>
+          <Switch>
               <Route path="/" exact component={Home} />
               <Route path="/login" component={Login} />
               <Route path="/login" component={SignUp} />
-           </Routes>
+           </Switch>
            {/* </AuthContext.Provider> */}
     </Router>
   );
