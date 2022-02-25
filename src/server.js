cbe48b3a3483fac4982feadead264824c342a4b1
Shortround <Longleechang@gmail.com>
2022-02-24 17:22:01 -0800

    test commit

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ async function main() {
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://leeec:leeec@csci152roadmap.aqfu4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
     
     const client = new MongoClient(uri);
  
