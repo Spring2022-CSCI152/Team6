8fa843683a3d0c8930d62a42454c149efd1d14cf
Michael Nugent <NugentMichael@live.com>
2022-03-14 23:47:53 -0700

    implemented email uniqueness in db check

--- src/test/Login.test.js
+++ src/test/Login.test.js
@@ -1,33 +1,39 @@
 import axios from '../axios';
 const User = require("../../backend/model/User");
 
-//sign up test
-test("Sign Up", () => {
+const user = {
+    "firstname": "TestFirst",
+    "lastname": "TestLast",
+    "email": "Test@Test.Test",
+    "password": "Test",
+};
 
-    // const {
-    //     firstname,
-    //     lastname,
-    //     email,
-    //     password
-    // } = {"firstName":"TestFirst", "TestLast" };
+//ensure the test user is in the roadmap mongodb collection "users" through an attempt to add him
+// beforeAll(async () => {
+//     return axios.post('/user/signup', user)
+//         .then((res) => {
+//             // console.log(res.data)
+//         }).catch((error) => {
+//             // console.log(error)
+//         });
+// });
 
-    const user = {
-        "firstname": "TestFirst",
-        "lastname": "TestLast",
-        "email": "Test@Test.Test",
-        "password": "Test",
-    };
-    // expect.assertions(1);
-    try {
-        expect(axios.post('/user/signup', user));
-    } catch (error) {
+//sign up test.  
+test("Sign Up: Check if email already registered to another user.", async () => {
 
-    }
+    //ensure the test user is in the roadmap mongodb collection "users" through an attempt to add him
+    //could move to beforeAll function
+    axios.post('/user/signup', user)
+        .then((res) => {
+            // console.log(res.data)
+        }).catch((error) => {
+            // console.log(error)
+        });
 
+    //store user's email into a variable
     let email = user.firstname;
-    let user1 = expect(User.findOne({
-        email
-    })).resolves;
-    if (!user1) throw new Error('no user found');
+
+    //check if user is found by email
+    expect(User.findOne({ email })).not.toBe(undefined)
 
 })
