3a5f7d7d225b569cd48bff08669d7a9ae2842b0d
Michael Nugent <NugentMichael@live.com>
2022-04-04 00:27:52 -0700

    moved axios function to profile.js

--- src/App.js
+++ src/App.js
@@ -15,20 +15,6 @@ import axios from 'axios';
 
 function App() {
 
-  axios.interceptors.request.use(
-    config => {
-      // const { origin } = new URL(config.url);
-      // const allowedOrigins = [apiUrl];
-      const token = localStorage.getItem('token');
-      // if (allowedOrigins.includes(origin)) {
-      config.headers.authorization = `Bearer ${token}`;
-      // }
-      return config;
-    },
-    error => {
-      return Promise.reject(error);
-    }
-  );
 
   // const { token, setToken } = useState(undefined);
 
