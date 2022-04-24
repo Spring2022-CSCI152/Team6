d79f85a00452503047f2a982f66ef36bc6eabddd
Michael Nugent <NugentMichael@live.com>
2022-04-24 14:35:53 -0700

    Tests for renders, text box update

--- src/pages/SearchCourse.js
+++ src/pages/SearchCourse.js
@@ -1,71 +1,68 @@
 import '../CSS/course.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from '../axios';
 
 function FindCourse() {
     const [findingcourses, setcourse] = useState("");
     const handleFinding = (e) => {
         setcourse(e.target.value)
-      }
+    }
     const search = async () => {
-        if(findingcourses == "")
-        {
+        if (findingcourses == "") {
             alert("Please input class for searching");
             return;
         }
-        const finding = {"classNameAb": findingcourses, "className": findingcourses};
+        const finding = { "classNameAb": findingcourses, "className": findingcourses };
         //sending to server
         const req = await axios.post('/course/search', finding)
             .then((res) => {
-            const classNameAb = document.getElementById("classNameAb");
-            classNameAb.innerHTML ="<p>Class Name Abbreviation: "+res.data.courses[0].classNameAb+"</p>";
+                const classNameAb = document.getElementById("classNameAb");
+                classNameAb.innerHTML = "<p>Class Name Abbreviation: " + res.data.courses[0].classNameAb + "</p>";
 
-            const className = document.getElementById("className");
-            className.innerHTML ="<p>Class Name: "+res.data.courses[0].className+"</p>";
+                const className = document.getElementById("className");
+                className.innerHTML = "<p>Class Name: " + res.data.courses[0].className + "</p>";
 
-            const prerequisites = document.getElementById("prerequisites");
-            prerequisites.innerHTML +="<p>Prerequisites:<p>";
-            for (let i of res.data.courses[0].Prerequisites)
-            {
-                prerequisites.innerHTML +="<li>"+ i +"</li>";
-            }
+                const prerequisites = document.getElementById("prerequisites");
+                prerequisites.innerHTML += "<p>Prerequisites:<p>";
+                for (let i of res.data.courses[0].Prerequisites) {
+                    prerequisites.innerHTML += "<li>" + i + "</li>";
+                }
 
-            const description = document.getElementById("description");
-            description.innerHTML ="<p>Description: "+res.data.courses[0].Description+"</p>";
+                const description = document.getElementById("description");
+                description.innerHTML = "<p>Description: " + res.data.courses[0].Description + "</p>";
 
-            const units = document.getElementById("units");
-            units.innerHTML ="<p>Units: "+res.data.courses[0].Units+"</p>";
+                const units = document.getElementById("units");
+                units.innerHTML = "<p>Units: " + res.data.courses[0].Units + "</p>";
 
-            const term = document.getElementById("term");
-            term.innerHTML +="<p>Course Typically Offered:<p>";
-            for (let i of res.data.courses[0].TermTypicallyOffered)
-            {
-                term.innerHTML +="<li>"+i+"</li>";
-            }
+                const term = document.getElementById("term");
+                term.innerHTML += "<p>Course Typically Offered:<p>";
+                for (let i of res.data.courses[0].TermTypicallyOffered) {
+                    term.innerHTML += "<li>" + i + "</li>";
+                }
 
             }).catch((error) => {
-            console.log(error);
+                console.log(error);
             });
-        }
+    }
 
-  return (
-    <div className="Course">
-        <form>
-            <label htmlFor='finding'>Search</label>
-            <input type="text" onChange={handleFinding} name="finding" className="finding" placeholder="finding..." id="finding" /> <br />
-            <button type="button" onClick={search}>Search</button>
-        </form>
-        <div id="result">
-            <p id="classNameAb"></p>
-            <p id="className"></p>
-            <div id="prerequisites"></div>
-            <p id="description"></p>
-            <p id="units"></p>
-            <p id="term"></p>
+    return (
+        <div className="Course">
+            <form>
+                <label htmlFor='finding'>Search</label>
+                <input type="text" onChange={handleFinding} name="finding" className="finding" placeholder="finding..." id="finding" value={findingcourses} /> <br />
+                <button type="button" name="submit" onClick={search}>Search</button>
+            </form>
+            <div id="result">
+                <p id="classNameAb"></p>
+                <p id="className"></p>
+                <div id="prerequisites"></div>
+                <p id="description"></p>
+                <p id="units"></p>
+                <p id="term"></p>
+            </div>
         </div>
-    </div>
 
-  );
+    );
 }
 
 export default FindCourse;
--- src/test/SearchCourse.test.js
+++ src/test/SearchCourse.test.js
@@ -10,19 +10,39 @@ configure({ adapter: new Adapter() });
 jest.mock('../axios');
 jest.mock('../CSS/course.css');
 
-describe('Search Course Function Test',  () => {
-it('Should ', async () => {
-    const wrapper = shallow(FindCourse)
+describe('SearchCourse Functional Component', () => {
+    const wrapper = shallow(<FindCourse />)
 
-    const finding = {"classNameAb": "", "className": ""};
+    it('should render a text input tag (search box)', () => {
+
+        expect(wrapper.find('input[name="finding"]').exists()).toBe(true);
+    });
+
+    it('should render a submit button', () =>{
+        expect(wrapper.find('button[name="submit"]').exists()).toBe(true);
+    })
+
+    it('the default value for text input box should be empty', () => {
+
+        expect(wrapper.find('input[name="finding"]').prop('value')).toBe("");
+    });
+
+    it('on change of value in the field, the state of that field in the component should be updated', () => {
+
+        //simulate text box entry of 'CSCI 1'
+        wrapper.find('input[name="finding"]').simulate('change', { target: { value: 'CSCI 1' } });
+
+        //verify change to state
+        expect(wrapper.find('input[name="finding"]').prop('value')).toBe('CSCI 1');
+    })
+
+
+    const finding = { "classNameAb": "", "className": "" };
 
     axios.post.mockResolvedValueOnce('');
 
-    await wrapper.instance();
+    // await wrapper.instance();
 
-    expect(axios.post).toHaveBeenCalledWith('/course/search', finding)
+    // expect(axios.post).toHaveBeenCalledWith('/course/search', finding)
 
 });
-    
-
-})
