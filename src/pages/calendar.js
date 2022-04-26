73043e2bdc0314daefbe9d64d92d35a9463dba97
gurleen227 <gurleenkaurvaid@gmail.com>
2022-04-26 14:59:00 -0700

    minors

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -12,18 +12,31 @@ function Calendar1() {
   // render() {
     const [date, setDate] = useState(new Date());
     return (
-      <div className='app' style = {{textAlign : "center"}}>
-        <Header></Header>
-      <h1 className='text-center'>React Calendar</h1>
-      <div className='calendar-container' style = {{textAlign : "center"}}>
-        <Calendar onChange={setDate} value={date} />
+      <div className='app'>
+        <h1 className='text-center'>React Calendar with Range</h1>
+        <div className='calendar-container'>
+          <Calendar
+            onChange={setDate}
+            value={date}
+            selectRange={true}
+          />
+        </div>
+        {date.length > 0 ? (
+          <p className='text-center'>
+            <span className='bold'>Start:</span>{' '}
+            {date[0].toDateString()}
+            &nbsp;|&nbsp;
+            <span className='bold'>End:</span> {date[1].toDateString()}
+          </p>
+        ) : (
+          <p className='text-center'>
+            <span className='bold'>Default selected date:</span>{' '}
+            {date.toDateString()}
+          </p>
+        )}
       </div>
-      <p className='text-center' style={{fontSize: "20px"}} >
-        <span className='bold' style = {{color: "white"}}>Selected Date:</span>{' '}
-        <span className='bold' style = {{color: "white"}}>{date.toDateString()}</span>
-      </p>
-    </div>
     );
   }
+  
 //}
 export default Calendar1;
