aba5649d90528415b4e621fb087a94403b7f437f
Michael Nugent <NugentMichael@live.com>
2022-04-27 14:55:15 -0700

    cleaning

--- src/pages/SearchCourse.js
+++ src/pages/SearchCourse.js
@@ -71,11 +71,6 @@ function FindCourse() {
             <form onSubmit={search} name="SearchCourseForm">
                 <label htmlFor='finding'>Search</label>
                 <input type="text" onChange={handleFinding} name="finding" className="finding" placeholder="finding..." id="finding" value={findingcourses} /> <br />
-                {/* <select className="field" name="field" id="field" onChange={setField}>
-                    <option value="code">Code</option>
-                    <option value="title">Title</option>
-                    <option 
-            </select> */}
                 <button type="submit" name="submit">Search</button>
             </form>
             <div id="result">
