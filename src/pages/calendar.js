c35e799a339fce79102c6092efedf5461a97627e
gurleen227 <gurleenkaurvaid@gmail.com>
2022-05-01 22:01:40 -0700

    changes to calendar

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -19,39 +19,6 @@ let styles = {
     borderWidth: "3px", //make outer edge of calendar thicker
   },
   
-<<<<<<< Updated upstream
-  // render() {
-    const [date, setDate] = useState(new Date());
-    return (
-      <div className='app'>
-        <h1 className='text-center'>React Calendar with Range</h1>
-        <div className='calendar-container'>
-          <Calendar
-            onChange={setDate}
-            value={date}
-            selectRange={true}
-          />
-        </div>
-        {date.length > 0 ? (
-          <p className='text-center'>
-            <span className='bold'>Start:</span>{' '}
-            {date[0].toDateString()}
-            &nbsp;|&nbsp;
-            <span className='bold'>End:</span> {date[1].toDateString()}
-          </p>
-        ) : (
-          <p className='text-center'>
-            <span className='bold'>Default selected date:</span>{' '}
-            {date.toDateString()}
-          </p>
-        )}
-      </div>
-    );
-  }
-  
-//}
-export default Calendar1;
-=======
   //you can also use emotion's string styles
   today: css`
    /* highlight today by making the text red and giving it a red border */
