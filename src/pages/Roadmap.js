b7a531a030d028f77c74e3c2ab5941704494d22b
vinguyen12 <36.tuongvi@gmail.com>
2022-05-07 13:43:11 -0700

    update roadmap

--- src/pages/Roadmap.js
+++ src/pages/Roadmap.js
@@ -12,7 +12,7 @@ function Roadmap() {
 
     const [matrix, setMatrix] = useState([]);
     // const [courseAlreadyTaken, setCourseAlreadyTaken] = useState([]);
-    var courseAlreadyTaken=["CSCI 40","CSCI 41","CSCI 60","MATH 75","MATH 75A"];
+    var courseAlreadyTaken=["MATH 75"];
     useEffect(() => {
 
         async function getUserInfoWrapper() {
@@ -57,12 +57,14 @@ function Roadmap() {
                 .then((res) => {
                     console.log(res);
                     for(let i of res.data.courses){
+                        var meetRequirement=true;
                         if(i.Prerequisites.length!=0)
                         {
                             OR=[];
                             console.log("has prerequesite",i.Prerequisites);
                             for (var element of i.Prerequisites)
                             {
+                                // console.log(element)
                                 if (element.includes(" (may be taken concurrently)")){
                                     element=element.replace(" (may be taken concurrently)","");
                                 }
@@ -70,31 +72,30 @@ function Roadmap() {
                                 {
                                     break;
                                 }
-                                console.log(courseAlreadyTaken)
                                 if((courseAlreadyTaken.filter((item) => item == element)[0]))
                                 {
+                                    meetRequirement = true;
                                     console.log("Meet the requirement for "+i.classNameAb + " with " + element);
                                 }
                                 else if(element.includes("OR"))
                                 {
                                     element=element.replace(" OR","");
                                     OR.push(element);
-                                    console.log(element)
                                 }
                                 else{
                                     meetRequirement = false;
                                     console.log("Doesn't the requirement for "+i.classNameAb + " with " + element);
+                                    break;
                                 }
                             }
-                            if(OR!=[] && meetRequirement==true && !element.includes("Permission from instructor OR"))
+                            if(OR!=[] && meetRequirement==true)
                             {
-                                console.log(OR);
+                                console.log("get in")
                                 for (const element of OR)
                                 {
-                                    console.log(typeof(element))
                                     var courseMeetORRequirement=courseAlreadyTaken.filter((item) => item == element);
-                                    console.log(courseMeetORRequirement)
-                                    if(courseMeetORRequirement)
+                                    console.log(courseMeetORRequirement.length)
+                                    if(courseMeetORRequirement.length!=0)
                                     {
                                         
                                         console.log("Meet the requirement for "+i.classNameAb + " with ",courseMeetORRequirement);
@@ -103,89 +104,40 @@ function Roadmap() {
                                     }
                                     meetRequirement = false;
                                     console.log("Doesn't meet the requirement for "+i.classNameAb + " because of ",element);
-
                                 }
                             }
-                            }
+                        }
+                        console.log(meetRequirement)
                         if(meetRequirement==true)
                         {
                             console.log(i,"Meet all requirement");
                             coursesOfferInSpring.push(i);
-                            const springTerm = document.getElementsByClassName('course-dropdown');
-                            Array.prototype.forEach.call (springTerm, function (node) {
-                                let fallOptions = document.createElement("option");
-                                fallOptions.text = i.classNameAb;
-                                fallOptions.value = i.classNameAb;
-                                node.appendChild(fallOptions);
+                            const term = document.getElementsByClassName('course-dropdown');
+                            Array.prototype.forEach.call (term, function (node) {
+                                let courseOptions = document.createElement("option");
+                                courseOptions.id="courses";
+                                courseOptions.text = i.classNameAb;
+                                courseOptions.value = i.classNameAb;
+                                node.appendChild(courseOptions);
                             } );
                         }
                         else
                         {
                             console.log(i,"Doesn't Meet all requirement");
                         }
-                        
-                            // coursesOfferInSpring.push(i);
-                            // const springTerm = document.getElementsByClassName('course-dropdown');
-                            // Array.prototype.forEach.call (springTerm, function (node) {
-                            //     let fallOptions = document.createElement("option");
-                            //     fallOptions.text = i.classNameAb;
-                            //     fallOptions.value = i.classNameAb;
-                            //     node.appendChild(fallOptions);
-                            // } );
-                            // coursesOfferInFall.push(i);
-                            // const fallTerm = document.querySelectorAll('[id=fall-dropdown]');
-                            // Array.prototype.forEach.call (fallTerm, function (node) {
-                            //     let fallOptions = document.createElement("option");
-                            //     fallOptions.text = i.classNameAb;
-                            //     fallOptions.value = i.classNameAb;
-                            //     node.appendChild(fallOptions);
-                            // } );
                     }
-                    
                 })
                 .catch((error) => {
                     console.log(error);
                 })
     }
-    // const filterSpring = async (e) => {
-    //     //sending to server
-    //     const req = await axios.post('/course/searchWithTermFilter', {"TermTypicallyOffered": "Spring"})
-    //         .then((res) => {
-    //             for(let i of res.data.courses){
-    //                 coursesOfferInSpring.push(i);
-    //                 const springTerm = document.getElementsByClassName('spring-dropdown');
-    //                 Array.prototype.forEach.call (springTerm, function (node) {
-    //                     let fallOptions = document.createElement("option");
-    //                     fallOptions.text = i.classNameAb;
-    //                     fallOptions.value = i.classNameAb;
-    //                     node.appendChild(fallOptions);
-    //                 } );
-    //             }
-    //             console.log("spring")
-    //         }).catch((error) => {
-    //             alert(error);
-    //         });    
-    // }    
 
-    // const filterFall = async (e) => {
-    //     //sending to server
-    //     const req = await axios.post('/course/searchWithTermFilter', {"TermTypicallyOffered": "Fall"})
-    //         .then((res) => {
-    //             for(let i of res.data.courses){
-    //                 coursesOfferInFall.push(i);
-    //                 const fallTerm = document.querySelectorAll('[id=fall-dropdown]');
-    //                 Array.prototype.forEach.call (fallTerm, function (node) {
-    //                     let fallOptions = document.createElement("option");
-    //                     fallOptions.text = i.classNameAb;
-    //                     fallOptions.value = i.classNameAb;
-    //                     node.appendChild(fallOptions);
-    //                 } );
-    //             }
-    //             console.log("fall")
-    //         }).catch((error) => {
-    //             alert(error);
-    //         });    
-    // }
+    function clearCourse(){
+        const term = document.getElementsByClassName('course-dropdown');
+        Array.prototype.forEach.call (term, function (node) {
+            node.innerHTML="";
+        } );
+    }
 
     document.getElementsByClassName("year-dropdown").onChange= function(e){
         console.log(e.target.value);
@@ -194,23 +146,19 @@ function Roadmap() {
         var td =document.getElementById(e.target.id).parentNode;
         td.innerHTML=e.target.value;
         var tr=td.parentNode;
-        console.log(tr)
         tr.parentNode.id =e.target.value; //tbody's id
         var newYear=[];
         newYear.push(e.target.value);
         setMatrix(newYear);
-        console.log(newYear);
-        console.log(matrix);
     }
     const addCourse = async (e) =>{
         console.log(e.target.value);
     }
     
-    var meetRequirement=true;
+
     var OR=[];
 
     const finalize = async (e) =>{
-        console.log(e.target.parentNode.parentNode.parentNode);
         if(e.target.parentNode.parentNode.parentNode.id==""){
             alert("Please choose an academic year first");
         }
@@ -222,8 +170,11 @@ function Roadmap() {
                 console.log(res.data.courses[0].Units);
                 e.target.parentNode.nextElementSibling.innerHTML=res.data.courses[0].Units.toString();
                 e.target.parentNode.innerHTML=e.target.value;
+                courseAlreadyTaken.push(e.target.value);
                 // setCourseAlreadyTaken([res.data.courses[0]]);
+                clearCourse();
                 viewCourse();
+                console.log(courseAlreadyTaken);
             }).catch((error) => {
                 console.log(error);
             });
